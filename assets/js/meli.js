3da0ae7e4c51b46afd899b001ae44d1c1f4fa05e
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2016-08-27 14:52:36 -0500

    Added Meli API

--- assets/js/meli.js
+++ assets/js/meli.js
@@ -1,17 +1,16 @@
-function call(url) {
-  url = "http://articulo.mercadolibre.com.co/MCO-425960963-botas-para-hombre-en-cuero-borcegas-negro-_JM?attribute=11000-2105d8e";
+function call() {
+  var url = document.getElementById("email").value;
   var reqURL = processURL(url);
-  console.log(reqURL);
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       var salida = JSON.parse(xhttp.responseText);
-      var newHTML = "<h1>" + salida.title + "</h1>";
-      newHTML += "<span>"+ salida.currency_id + " " + salida.price + "</span><br>";
-      newHTML += "<span>"+ salida.condition + "</span><br>";
-      newHTML += "<span>"+ salida.permalink + "</span><br>";
-      newHTML += "<span>"+ salida.thumbnail + "</span><br>";
-      newHTML += "<span>"+ salida.price + "</span><br>";
+      var newHTML = "<center><h1>" + salida.title + "</h1><center>";
+      newHTML += "<span>precio: "+ salida.currency_id + " " + salida.price + "</span><br>";
+      var est = salida.condition == "new" ? "nuevo" : "usado";
+      newHTML += "<span>estado: "+ est + "</span><br>";
+      newHTML += "<span><a href='"+salida.permalink+"'> link mercadolibre </a></span><br>";
+      newHTML += "<img src='"+salida.thumbnail+"'><br>";
 
       var images = salida.pictures;
       for(var i=0; i < images.length; i++){
@@ -29,13 +28,9 @@ function call(url) {
 
 
 function processURL (url){
-  console.log(url);
   var subUrl = url.split("mercadolibre.com.co/")[1].split("-");
-  console.log(subUrl[0]);
-  console.log(subUrl[1]);
   var resturl;
   resturl = "" +  subUrl[0] + "" + subUrl[1];
-  console.log(resturl);
   return "https://api.mercadolibre.com/items/" + resturl;
 
 }
